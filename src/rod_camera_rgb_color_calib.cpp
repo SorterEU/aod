e9e19c8d07156af36c63188708272e42920e2272
jfigat <jfigat@6bd8010f-f003-4847-87ea-4b09c54e78b1>
2014-06-24 16:42:27 +0000

    Color Calib
    git-svn-id: https://robotyka.ia.pw.edu.pl/svn/sorter/src/aod@206 6bd8010f-f003-4847-87ea-4b09c54e78b1

--- src/rod_camera_rgb_color_calib.cpp
+++ src/rod_camera_rgb_color_calib.cpp
@@ -1,10 +1,11 @@
 #include "NodeClass.h"
 #include <aod/ColorCalibConfig.h>
+#include "ColorCalibrationDll_include.h"// Color Calibration
 
 class ColorCalib {
 public:
 	ColorCalib(){
-		sub_=nh_.subscribe("/rod/camera/rgb/image_color", 1000, &ColorCalib::messageCallbackImageRectColor, this);
+        sub_=nh_.subscribe("/rod/camera/rgb/image_color", 1, &ColorCalib::messageCallbackImageRectColor, this);
 		pub_message_ = nh_.advertise<sensor_msgs::Image>("/rod/camera/rgb/image_calibrated_color", 1000);
 	}
 	~ColorCalib(){}
@@ -13,13 +14,49 @@ public:
 	{ 
 	 
 	  ROS_INFO("Reconfigure request, parameter: %f",
-	           config.parameter);
+               config.parameter);
 	}
 	
-	void messageCallbackImageRectColor( const sensor_msgs::Image::ConstPtr& msg)
+    void messageCallbackImageRectColor( const sensor_msgs::ImageConstPtr& msg)//const sensor_msgs::Image::ConstPtr& msg)
 	{
-		ROS_INFO("sensor_msgs::Image");
-		//todo
+        ROS_INFO("sensor_msgs::Image");
+        //todo
+
+
+        cv::Mat src;
+        float calib_corr_R, calib_corr_G, calib_corr_B;
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+
+        src=cv_ptr->image;
+
+            //Test
+    //    cv::Mat roi(src, cv::Rect(10,10,100,100));
+    //    cv::Mat roi2(src, cv::Rect(100,100,900,800));
+    //    cv::namedWindow("ROI"); //create a display window
+    //    cv::imshow("ROI", roi2);
+    //    cv::waitKey(1);
+            //Test
+
+        myColorCalib::col_cal_compute(src, cv_ptr->image, calib_corr_R, calib_corr_G, calib_corr_B,true);
+        myColorCalib::col_cal_use(src,cv_ptr->image,calib_corr_R*0.6,calib_corr_G*0.6,calib_corr_B*0.6,true);
+
+    //        //Test
+    //    cv::namedWindow("Calibrated"); //create a display window
+    //    cv::imshow("Calibrated", cv_ptr->image);
+    //    cv::waitKey(3);//3
+    //        //~Test
+
+//        publishMessageImageCalibratedColor(&pub_message_,*(cv_ptr->toImageMsg()));
+        pub_message_.publish(*(cv_ptr->toImageMsg()));
 		
 	}
 		
