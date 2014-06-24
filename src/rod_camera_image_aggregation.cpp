24258fb72b11f2fb004b0f53cd1a69a5575316dd
mfigat <mfigat@6bd8010f-f003-4847-87ea-4b09c54e78b1>
2014-06-24 14:37:32 +0000

    dodane
    git-svn-id: https://robotyka.ia.pw.edu.pl/svn/sorter/src/aod@195 6bd8010f-f003-4847-87ea-4b09c54e78b1

--- src/rod_camera_image_aggregation.cpp
+++ src/rod_camera_image_aggregation.cpp
@@ -1,16 +1,81 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+
 #include "NodeClass.h"
-
-#include <sstream>
-
-
-//globals:
-
-void image_agregation();
+#include <aod/ImageAggregationConfig.h>
 
 
-void image_agregation(){}
+class ImageAggregation {
+public:
+	ImageAggregation(){
+		sub_color_calib_ = nh_.subscribe("/rod/camera/rgb/image_calibrated_color", 1000, &ImageAggregation::messageCallbackImageCalibratedColor, this);
+		sub_rgb_STFP_= nh_.subscribe("/TF", 1000, &ImageAggregation::messageCallbackTF, this);
+		sub_ir_STFP_= nh_.subscribe("/TF", 1000, &ImageAggregation::messageCallbackTF, this);
+		sub_image_proc_= nh_.subscribe("/rod/camera/ir/image_mono", 1000, &ImageAggregation::messageCallbackImageRectMono, this);
+		  
+		pub_message_ = nh_.advertise<std_msgs::String>("/rod/analysis/streamIN", 1000);
+	}
+	~ImageAggregation(){}
+	
+	void callback(aod::ImageAggregationConfig &config, uint32_t level)
+	{ 
+	 
+	  ROS_INFO("Reconfigure request, parameter: %f",
+	           config.parameter);
+	}
+	
+	void messageCallbackTF( const std_msgs::String::ConstPtr& msg)
+	{
+		ROS_INFO("message in messageCallbackTF");
+		
+		std_msgs::String tmp_msg_;
+		std::stringstream ss;
+		ss << "StreamIN ";
+		tmp_msg_.data = ss.str();
+		ROS_INFO("%s", tmp_msg_.data.c_str());
+
+		pub_message_.publish(tmp_msg_);
+
+	}
+	
+	void messageCallbackImageRectMono( const sensor_msgs::Image::ConstPtr& msg)
+	{
+		ROS_INFO("message in messageCallbackImageRectMono");
+		
+		std_msgs::String tmp_msg_;
+		std::stringstream ss;
+		ss << "StreamIN ";
+		tmp_msg_.data = ss.str();
+		ROS_INFO("%s", tmp_msg_.data.c_str());
+
+		pub_message_.publish(tmp_msg_);
+
+	}
+	
+	void messageCallbackImageCalibratedColor( const sensor_msgs::Image::ConstPtr& msg)
+	{
+		ROS_INFO("message in messageCallbackImageCalibratedColor");
+		
+		std_msgs::String tmp_msg_;
+		std::stringstream ss;
+		ss << "StreamIN ";
+		tmp_msg_.data = ss.str();
+		ROS_INFO("%s", tmp_msg_.data.c_str());
+
+		pub_message_.publish(tmp_msg_);
+
+	}
+	
+	
+	
+private:
+	ros::Publisher pub_message_;
+	ros::NodeHandle nh_;
+	ros::Subscriber sub_image_proc_;
+	ros::Subscriber sub_ir_STFP_;
+	ros::Subscriber sub_rgb_STFP_;
+	ros::Subscriber sub_color_calib_;
+};
+
+	
 
 
 int main(int argc, char **argv)
@@ -19,25 +84,17 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "image_aggregation");
   ros::NodeHandle n;
   
-  //Create a new NodeClass object
-  NodeClass *node_class = new NodeClass();
+  dynamic_reconfigure::Server<aod::ImageAggregationConfig> srv;
+  dynamic_reconfigure::Server<aod::ImageAggregationConfig>::CallbackType f;
+    
+  ImageAggregation imageAggregation_;
   
-  // Create a publisher and name the topic.
-  node_class->_pub_message = n.advertise<std_msgs::String>("/rod/analysis/streamIN", 1000);
-
-  // Tell ROS how fast to run this node.
-  ros::Rate loop_rate(10); 
-  
-  //subscribes
-  ros::Subscriber sub_color_calib = n.subscribe("/rod/camera/rgb/image_calibrated_color", 1000, &NodeClass::messageCallbackImageCalibratedColor, node_class);
-  ros::Subscriber sub_rgb_STFP= n.subscribe("/TF", 1000, &NodeClass::messageCallbackTF, node_class);
-  ros::Subscriber sub_ir_STFP= n.subscribe("/TF", 1000, &NodeClass::messageCallbackTF, node_class);
-  ros::Subscriber sub_image_proc= n.subscribe("/rod/camera/ir/image_mono", 1000, &NodeClass::messageCallbackImageRectMono, node_class);
-  
-  
+  f = boost::bind(&ImageAggregation::callback, &imageAggregation_, _1, _2);
+  srv.setCallback(f);
+    
+  ros::Rate loop_rate(10);
+  ROS_INFO("Starting to spin...");
   ros::spin();
-
-
 
   return 0;
 }
